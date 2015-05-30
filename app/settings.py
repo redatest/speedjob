dffa268c45ff90695eba38e5708ff1f6fbba6634
Med reda <redatest7@gmail.com>
2015-05-30 02:05:41 +0200

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -15,11 +15,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'db549366168',         # Or path to database file if using sqlite3.
-        'USER': 'dbo549366168',                      # Not used with sqlite3.
-        'PASSWORD': 'guigui22',                  # Not used with sqlite3.
-        'HOST': 'db549366168.db.1and1.com',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '3306',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': '',         # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
